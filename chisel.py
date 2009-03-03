16e92ef8798a7e1082759fea267f83cfc5ff9f52
David Zhou <david@nodnod.net>
2009-03-02 21:11:42 -0500

    initial version

---
+++ chisel.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+# Chisel
+# David Zhou
+# 
+# Requires:
+# jinja2
+
+import sys, re, time, os
+import jinja2, markdown
+
+#Settings
+SOURCE = "./blog/" #end with slash
+DESTINATION = "./export/" #end with slash
+HOME_SHOW = 15 #numer of entries to show on homepage
+TEMPLATE_PATH = "./templates/"
+TEMPLATE_OPTIONS = {}
+TEMPLATES = {
+    'home': "home.html",
+    'detail': "detail.html",
+    'archive': "archive.html",
+}
+TIME_FORMAT = "%B %d, %Y - %I:%M %p"
+
+#FORMAT should be a callable that takes in text
+#and returns formatted text
+FORMAT = lambda text: markdown.markdown(text, ['footnotes',]) 
+#########
+
+
+STEPS = []
+
+def step(func):
+    def wrapper(*args, **kwargs):
+        print "Starting " + func.__name__ + "...",
+        func(*args, **kwargs)
+        print "Done."
+    STEPS.append(wrapper)
+    return wrapper
+
+def get_tree(source):
+    files = []
+    for root, ds, fs in os.walk(source):
+        for name in fs:
+            path = os.path.join(root, name)
+            epoch = os.path.getmtime(path)
+            date = time.localtime(epoch)
+            year, month, day = map(int, date[:3])
+            f = open(path, "r")
+            files.append({
+                'title': f.readline(),
+                'content': FORMAT(''.join(f.readlines()[1:])),
+                'url': '/'.join([str(year), "%.2d" % month, "%.2d" % day, os.path.splitext(name)[0] + ".html"]),
+                'date': time.strftime(TIME_FORMAT, date),
+                'epoch': epoch,
+                'year': year,
+                'month': month,
+                'day': day,
+            })
+            f.close()
+    return files
+
+def write_file(url, data):
+    path = DESTINATION + url
+    dirs = os.path.dirname(path)
+    if not os.path.isdir(dirs):
+        os.makedirs(dirs)
+    file = open(path, "w")
+    file.write(data)
+    file.close()
+
+@step
+def generate_homepage(f, e):
+    """Generate homepage"""
+    template = e.get_template(TEMPLATES['home'])
+    write_file("index.html", template.render(entries=f[:HOME_SHOW]))
+
+@step
+def master_archive(f, e):
+    """Generate master archive list of all entries"""
+    template = e.get_template(TEMPLATES['archive'])
+    write_file("archives.html", template.render(entries=f))
+
+@step
+def detail_pages(f, e):
+    """Generate detail pages of individual posts"""
+    template = e.get_template(TEMPLATES['detail'])
+    for file in f:
+        write_file(file['url'], template.render(file))
+
+@step
+def date_indices(f, e):
+    """
+    Generate date indices for all year, month, days
+    permutations. Example: "/2009/02/"
+    """
+    pass
+
+
+def main():
+    print "Chiseling..."
+    print "\tReading files...",
+    files = sorted(get_tree(SOURCE), lambda x,y: not cmp(x['epoch'], y['epoch']))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_PATH), **TEMPLATE_OPTIONS)
+    print "Done."
+
+    print "\tRunning steps..."
+    for step in STEPS:
+        print "\t\t",
+        step(files, env)
+    print "\tDone."
+    print "Done."
+
+if __name__ == "__main__":
+    sys.exit(main())

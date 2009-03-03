d942d31e71096e45926cdf64b0e1fe386ccd47d6
David Zhou <david@nodnod.net>
2009-03-02 21:50:15 -0500

    fixed typo in detail view

--- chisel.py
+++ chisel.py
@@ -86,7 +86,7 @@ def detail_pages(f, e):
     """Generate detail pages of individual posts"""
     template = e.get_template(TEMPLATES['detail'])
     for file in f:
-        write_file(file['url'], template.render(entry=f))
+        write_file(file['url'], template.render(entry=file))
 
 @step
 def date_indices(f, e):

4d8a6d9531cb9fb8664f8e6ebde1bf13bdbd5f2d
Ronan Jouchet <ronan@jouchet.fr>
2012-04-15 16:13:32 -0300

    * look for the date in the filename instead of the first line * generate RSS feed with PyRSS2Gen

--- chisel.py
+++ chisel.py
@@ -4,15 +4,17 @@
 # David Zhou
 # 
 # Requires:
-# jinja2
+# jinja2, PyRSS2Gen
 
 import sys, re, time, os, codecs
-import jinja2, markdown
+import jinja2, markdown, PyRSS2Gen
+import datetime
 
 #Settings
-SOURCE = "./blog/" #end with slash
-DESTINATION = "./export/" #end with slash
-HOME_SHOW = 15 #numer of entries to show on homepage
+BASEURL = "http://www.flyingmolehill.com/" #end with slash
+SOURCE = "../../Blog-Posts/" #end with slash
+DESTINATION = "../../Blog-Published/" #end with slash
+HOME_SHOW = 20 #numer of entries to show on homepage
 TEMPLATE_PATH = "./templates/"
 TEMPLATE_OPTIONS = {}
 TEMPLATES = {
@@ -21,10 +23,17 @@ TEMPLATES = {
     'archive': "archive.html",
 }
 TIME_FORMAT = "%B %d, %Y"
-ENTRY_TIME_FORMAT = "%m/%d/%Y"
+ENTRY_TIME_FORMAT = "%Y-%m-%d"
 #FORMAT should be a callable that takes in text
 #and returns formatted text
-FORMAT = lambda text: markdown.markdown(text, ['footnotes',]) 
+FORMAT = lambda text: markdown.markdown(text, ['footnotes',])
+RSS = PyRSS2Gen.RSS2(
+    title = "ronj",
+    link = BASEURL + "feed.xml",
+    description = "{information, music} nerd",
+    lastBuildDate = datetime.datetime.now(),
+    items = [])
+
 #########
 
 STEPS = []
@@ -44,14 +53,16 @@ def get_tree(source):
             if name[0] == ".": continue
             path = os.path.join(root, name)
             f = open(path, "rU")
-            title = f.readline().decode('UTF-8')
-            date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
+            title = f.readline().decode('UTF-8')[:-1]
+            date = time.strptime(os.path.splitext(name)[0][:10], ENTRY_TIME_FORMAT) #date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
             year, month, day = date[:3]
+            content = FORMAT(''.join(f.readlines()[1:]).decode('UTF-8'))
+            url = '/'.join([str(year), "%.2d" % month, "%.2d" % day, os.path.splitext(name)[0][11:] + ".html"])
             files.append({
                 'title': title,
                 'epoch': time.mktime(date),
-                'content': FORMAT(''.join(f.readlines()[1:]).decode('UTF-8')),
-                'url': '/'.join([str(year), "%.2d" % month, "%.2d" % day, os.path.splitext(name)[0] + ".html"]),
+                'content': content,
+                'url': url,
                 'pretty_date': time.strftime(TIME_FORMAT, date),
                 'date': date,
                 'year': year,
@@ -82,6 +93,13 @@ def generate_homepage(f, e):
     """Generate homepage"""
     template = e.get_template(TEMPLATES['home'])
     write_file("index.html", template.render(entries=f[:HOME_SHOW]))
+
+@step
+def generate_rss(f, e):
+    """Generate rss"""
+    for file in f[:10]:
+        RSS.items.append(PyRSS2Gen.RSSItem(title=file['title'], link=BASEURL + file['url'], description=file['content'], author="Ronan", guid = PyRSS2Gen.Guid(BASEURL + file['url']), pubDate=datetime.datetime(file['year'], file['month'], file['day'])))
+    RSS.write_xml(open(DESTINATION + "feed.xml", "w"))
 
 @step
 def master_archive(f, e):

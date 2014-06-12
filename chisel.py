3c573264e9e46b99f1b41c700cb1660265f1d232
ckunte <chetan@ckunte.net>
2014-06-12 10:40:21 +0800

    Numerious updates (listed below)
    - RSS feed generator script using PyRSS2Gen (Hat tip: @ronjouch)
    - A shorter year based permalink structure.
    - Added an option for clean URLs.
    - Smartypants content parsing.
    - Removed redundant code.
    - Simplified step template definitions.
    - Additional shorter date formats included for archive listing.

--- chisel.py
+++ chisel.py
@@ -1,40 +1,45 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
+# encoding: utf-8
 # Chisel
-# David Zhou
-# 
-# Requires:
-# jinja2, PyRSS2Gen
+# David Zhou, github.com/dz
+# Chyetanya Kunte, github.com/ckunte
+try:
+    import sys, os, re, time, jinja2, markdown, mdx_smartypants, PyRSS2Gen, datetime
+except ImportError as error:
+    print 'ImportError:', str(error)
+    exit(1)
 
-import sys, re, time, os, codecs
-import jinja2, markdown, PyRSS2Gen
-import datetime
-
-#Settings
-BASEURL = "http://www.flyingmolehill.com/" #end with slash
-SOURCE = "../../Blog-Posts/" #end with slash
-DESTINATION = "../../Blog-Published/" #end with slash
-HOME_SHOW = 20 #numer of entries to show on homepage
-TEMPLATE_PATH = "./templates/"
-TEMPLATE_OPTIONS = {}
-TEMPLATES = {
-    'home': "home.html",
-    'detail': "detail.html",
-    'archive': "archive.html",
-}
-TIME_FORMAT = "%B %d, %Y"
-ENTRY_TIME_FORMAT = "%Y-%m-%d"
-#FORMAT should be a callable that takes in text
-#and returns formatted text
-FORMAT = lambda text: markdown.markdown(text, ['footnotes',])
+# BEGIN SETTINGS
+BASEURL = "http://ckunte.net/log/" #trailing slash
+SOURCE = "../posts/" #trailing slash
+DESTINATION = "../www/" #trailing slah
+TEMPLATE_PATH = "./templates/" #trailing slash
+HOME_SHOW = 3 # number of entries to show on homepage
+URLEXT = "" # for clean urls (else use: ".html")
+# Date formats
+TIME_FORMAT = "%B %-d, %Y" # shown on post entries
+STIME_FORMAT = "%b %d" # shown on the archive page
+ENTRY_TIME_FORMAT = "%m/%d/%Y" # to be input on the second line of post in markdown
+# RSS details
 RSS = PyRSS2Gen.RSS2(
-    title = "ronj",
-    link = BASEURL + "feed.xml",
-    description = "{information, music} nerd",
+    title = "Chyetanya Kunte", # Blog title
+    description = "ckunte.net/log.", # Blog description
+    link = BASEURL + "rss.xml", 
     lastBuildDate = datetime.datetime.now(),
     items = [])
 
-#########
+# END SETTINGS
+#
+TEMPLATE_OPTIONS = {}
+#
+FORMAT = lambda text: markdown.markdown(text, ['smartypants','footnotes'])
+#
+if URLEXT == ".html":
+    PATHEXT = ""
+    pass
+if URLEXT == "":
+    PATHEXT = ".html"
+    pass
 
 STEPS = []
 
@@ -51,73 +56,63 @@ def get_tree(source):
     for root, ds, fs in os.walk(source):
         for name in fs:
             if name[0] == ".": continue
+            if not re.match(r'^.+\.(md|mdown|markdown)$', name): continue
             path = os.path.join(root, name)
-            f = open(path, "rU")
-            title = f.readline().decode('UTF-8')[:-1]
-            date = time.strptime(os.path.splitext(name)[0][:10], ENTRY_TIME_FORMAT) #date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
-            year, month, day = date[:3]
-            content = FORMAT(''.join(f.readlines()[1:]).decode('UTF-8'))
-            url = '/'.join([str(year), "%.2d" % month, "%.2d" % day, os.path.splitext(name)[0][11:] + ".html"])
-            files.append({
-                'title': title,
-                'epoch': time.mktime(date),
-                'content': content,
-                'url': url,
-                'pretty_date': time.strftime(TIME_FORMAT, date),
-                'date': date,
-                'year': year,
-                'month': month,
-                'day': day,
-                'filename': name,
-            })
-            f.close()
+            with open(path, "rU") as f:
+                title = f.readline().decode('UTF-8')[:-1].strip('\n\t')
+                date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
+                year, month, day = date[:3]
+                content = FORMAT(''.join(f.readlines()[1:]).decode('UTF-8'))
+                url = '/'.join([str(year), os.path.splitext(name)[0] + URLEXT])
+                files.append({
+                    'title': title,
+                    'epoch': time.mktime(date),
+                    'content': content,
+                    'url': url,
+                    'pretty_date': time.strftime(TIME_FORMAT, date),
+    				'sdate': time.strftime(STIME_FORMAT, date),
+                    'date': date,
+                    'year': year,
+                    'month': month,
+                    'day': day,
+                    'filename': name})
     return files
 
-def compare_entries(x, y):
-    result = cmp(-x['epoch'], -y['epoch'])
-    if result == 0:
-        return -cmp(x['filename'], y['filename'])
-    return result
-
 def write_file(url, data):
-    path = DESTINATION + url
+    path = DESTINATION + url + PATHEXT
     dirs = os.path.dirname(path)
-    if not os.path.isdir(dirs):
-        os.makedirs(dirs)
-    file = open(path, "w")
-    file.write(data.encode('UTF-8'))
-    file.close()
+    if not os.path.isdir(dirs): os.makedirs(dirs)
+    with open(path, "w") as file:
+        file.write(data.encode('UTF-8'))
+
+def write_feed(url, data):
+    path = DESTINATION + url
+    with open(path, "w") as file:
+        file.write(data.encode('UTF-8'))
 
 @step
-def generate_homepage(f, e):
-    """Generate homepage"""
-    template = e.get_template(TEMPLATES['home'])
-    write_file("index.html", template.render(entries=f[:HOME_SHOW]))
+def gen_rss(f, e):
+    for file in f[:3]:
+        RSS.items.append(PyRSS2Gen.RSSItem(title=file['title'], link=BASEURL + file['url'], description=file['content'], author="Chyetanya Kunte", guid = PyRSS2Gen.Guid(BASEURL + file['url']), pubDate=datetime.datetime(file['year'], file['month'], file['day'])))
+    RSS.write_xml(open(DESTINATION + "rss.xml", "w"))
 
 @step
-def generate_rss(f, e):
-    """Generate rss"""
-    for file in f[:10]:
-        RSS.items.append(PyRSS2Gen.RSSItem(title=file['title'], link=BASEURL + file['url'], description=file['content'], author="Ronan", guid = PyRSS2Gen.Guid(BASEURL + file['url']), pubDate=datetime.datetime(file['year'], file['month'], file['day'])))
-    RSS.write_xml(open(DESTINATION + "feed.xml", "w"))
+def gen_home(f, e):
+    write_file('index' + URLEXT, e.get_template('home.html').render(entries=f[:HOME_SHOW]))
 
 @step
-def master_archive(f, e):
-    """Generate master archive list of all entries"""
-    template = e.get_template(TEMPLATES['archive'])
-    write_file("archives.html", template.render(entries=f))
-
-@step
-def detail_pages(f, e):
-    """Generate detail pages of individual posts"""
-    template = e.get_template(TEMPLATES['detail'])
+def gen_detailpages(f, e):
     for file in f:
-        write_file(file['url'], template.render(entry=file))
+        write_file(file['url'], e.get_template('detail.html').render(entry=file))    
+
+@step
+def gen_archive(f, e):
+    write_file('archive' + URLEXT, e.get_template('archive.html').render(entries=f))
 
 def main():
     print "Chiseling..."
     print "\tReading files...",
-    files = sorted(get_tree(SOURCE), cmp=compare_entries)
+    files = sorted(get_tree(SOURCE), key=lambda entry: entry['epoch'], reverse=True)
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_PATH), **TEMPLATE_OPTIONS)
     print "Done."
     print "\tRunning steps..."
